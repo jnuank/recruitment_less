5a8d7a94bae7d7d73c3cca24a8cd2d775937d4e4
Jun Nakajima <ikuta1919@gmail.com>
2022-05-06 11:26:18 +0900

    固定値で候補者の値を返す

--- recruitment_api/src/domain/candidate.rs
+++ recruitment_api/src/domain/candidate.rs
@@ -1,4 +1,6 @@
-#[derive(Debug, Clone)]
+use serde::Serialize;
+
+#[derive(Debug, Clone, Serialize)]
 pub struct Candidate {
     pub(crate) name: String,
     pub(crate) age: u32,
--- recruitment_api/src/routers.rs
+++ recruitment_api/src/routers.rs
@@ -1,7 +1,18 @@
-use axum::{Router, routing::get};
+use axum::response::IntoResponse;
+use axum::Json;
+use axum::{Router, routing::{get, Route}};
+use hyper::StatusCode;
+
+use crate::domain;
 
 
 
 pub fn router() -> Router {
-    Router::new().route("/ping", get(|| async { "pong" }))
+     Router::new().route("/ping", get(|| async {"ping"}))
+                    .route("/candidate", get( get_candidate))
+}
+
+async fn get_candidate() -> impl IntoResponse {
+    let candidate  = domain::candidate::Candidate { name: "bob".to_string(), age: 20 };
+    (StatusCode::CREATED, Json(candidate))
 }
